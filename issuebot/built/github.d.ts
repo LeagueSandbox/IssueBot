fa2227ab0e5a73adeff227da8ff7e7823ec83700
WaveHack <email@wavehack.net>
2018-08-04 20:58:05 +0200

    Cleanup .gitignore and ignore built directory, fixes #42

--- issuebot/built/github.d.ts
+++
@@ -1,6 +0,0 @@
-import GitHubApi = require('github');
-export declare class GitHub {
-    api: GitHubApi;
-    constructor(token: string);
-    submitIssue(owner: any, repo: any, title: any, description: any): Promise<any>;
-}
--- issuebot/built/issuebot.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Harmony from 'discord-harmony';
-import { GitHub } from './github';
-export declare class IssueBot extends Harmony.Bot {
-    gitHub: GitHub;
-    constructor();
-    loadCommands(): void;
-}
-declare var _default: IssueBot;
-export default _default;
